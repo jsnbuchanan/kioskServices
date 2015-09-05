bcbc3b840635056962aa6e8496b7970675f3835e
jasonb22 <jason.buchanan@us.usana.com>
2015-09-05 00:52:00 -0600

    Adding KioskServicesConfig to configure spring boot.

---
+++ src/main/java/hello/KioskServicesConfig.java
@@ -0,0 +1,11 @@
+package com.geekatplay.kiosk.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskServicesConfig {
+	public static void main(String[] args) {
+		SpringApplication.run(KioskServicesConfig.class, args); 
+	}
+}
