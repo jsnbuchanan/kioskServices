1488aacf94d63273a8b05527bfab399da25f4f76
jasonb22 <jason.buchanan@us.usana.com>
2015-09-05 14:46:20 -0600

    Adding CompositeController and basic model for initial services.

---
+++ src/main/java/com/geekatplay/kiosk/services/model/Background.groovy
@@ -0,0 +1,9 @@
+package com.geekatplay.kiosk.services.model
+
+class Background {
+
+    def long id
+    def String smoke
+    def String backdrop
+    def String border
+}
---
+++ src/main/java/com/geekatplay/kiosk/services/model/Foreground.groovy
@@ -0,0 +1,10 @@
+package com.geekatplay.kiosk.services.model
+
+class Foreground {
+
+    def long id
+    def String object
+    def String logo
+    def String smoke
+    def String border
+}
